fa339a48b87908a57119df76b9c926f3948be2ae
mabruelisauer <112378881+mabruelisauer@users.noreply.github.com>
2023-02-27 11:56:20 +0100

    added genericstack that also works with string ect.

---
+++ Stack/MyGenericStack.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.ConstrainedExecution;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stack
+{
+    public class MyGenericStack<T>
+    {
+        /// <summary>
+        /// Defines the size of the stack
+        /// </summary>
+        public int Size { get; set; }
+        /// <summary>
+        /// Shows the index of the highest element in the stack
+        /// </summary>
+        private int index { get; set; } = -1;
+        /// <summary>
+        /// Is the stack itself
+        /// </summary>
+        private T[] data;
+        public MyGenericStack(int size)
+        {
+            Size = size;
+            data = new T[Size];
+        }
+
+        /// <summary>
+        /// Adds a value to the Stack
+        /// </summary>
+        /// <param name="value">Takes an int as value to add</param>
+        /// <exception cref="Exception">Exception if maximum is reached</exception>
+        public void push(T value)
+        {
+            if (index + 1 == Size)
+            {
+                throw new Exception("Array has reached its maximum");
+            }
+            index++;
+            data[index] = value;
+        }
+
+        /// <summary>
+        /// Takes the highest element of the stack and removes it
+        /// </summary>
+        /// <returns>Returns the value of the element</returns>
+        public T Pop()
+        {
+            var lastNumber = data[index];
+            index--;
+            return lastNumber;
+        }
+
+        /// <summary>
+        /// Removes every element in the stack
+        /// </summary>
+        public void Clear()
+        {
+            Array.Clear(data, 0, Size);
+            index = -1;
+        }
+
+        /// <summary>
+        /// Swaps the two highest elements of the stack
+        /// </summary>
+        public void SwappXY()
+        {
+            var leastElement = data[index];
+            var secondLeastElement = data[index - 1];
+            data[index - 1] = leastElement;
+            data[index] = secondLeastElement;
+        }
+
+        /// <summary>
+        /// Checks if stack is empty
+        /// </summary>
+        /// <returns>Returns true or false</returns>
+        public bool isEmpty()
+        {
+            if (index == -1)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        /// <summary>
+        /// Checks if stack is full
+        /// </summary>
+        /// <returns>Returns true or false</returns>
+        public bool isFull()
+        {
+            if (index + 1 == Size)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        /// <summary>
+        /// Puts the whole stack inside of a string seperated with commas
+        /// </summary>
+        /// <returns>Returns the string</returns>
+        public string ToString()
+        {
+            return string.Join(", ", data);
+        }
+    }
+}
--- Stack/Program.cs
+++ Stack/Program.cs
@@ -4,24 +4,60 @@
     {
         static void Main(string[] args)
         {
-            MyStack myStack = new MyStack(4);
-            myStack.push(15);
-            myStack.push(8);
-            myStack.push(2);
-            myStack.isEmpty();
-            myStack.Pop();
-            myStack.push(3);
-            myStack.Clear();
-            myStack.isEmpty();
-            myStack.push(69);
-            myStack.push(45);
-            myStack.push(90);
-            myStack.isFull();
-            myStack.SwappXY();
-            myStack.push(40);
-            myStack.isFull();
-            myStack.Peek();
-            Console.WriteLine(myStack.ToString());
+            //MyStack myStack = new MyStack(4);
+            //myStack.push(15);
+            //myStack.push(8);
+            //myStack.push(2);
+            //myStack.isEmpty();
+            //myStack.Pop();
+            //myStack.push(3);
+            //myStack.Clear();
+            //myStack.isEmpty();
+            //myStack.push(69);
+            //myStack.push(45);
+            //myStack.push(90);
+            //myStack.isFull();
+            //myStack.SwappXY();
+            //myStack.push(40);
+            //myStack.isFull();
+            //myStack.Peek();
+            //Console.WriteLine(myStack.ToString());
+
+            //var genStack = new MyGenericStack<int>(4);
+            //genStack.push(15);
+            //genStack.push(8);
+            //genStack.push(2);
+            //genStack.isEmpty();
+            //genStack.Pop();
+            //genStack.push(3);
+            //genStack.Clear();
+            //genStack.isEmpty();
+            //genStack.push(69);
+            //genStack.push(45);
+            //genStack.push(90);
+            //genStack.isFull();
+            //genStack.SwappXY();
+            //genStack.push(40);
+            //genStack.isFull();
+            //Console.WriteLine(genStack.ToString());
+
+            var genStack = new MyGenericStack<string>(4);
+            genStack.push("a");
+            genStack.push("a");
+            genStack.push("a");
+            genStack.isEmpty();
+            genStack.Pop();
+            genStack.push("a");
+            genStack.Clear();
+            genStack.isEmpty();
+            genStack.push("a");
+            genStack.push("b");
+            genStack.push("c");
+            genStack.isFull();
+            genStack.SwappXY();
+            genStack.push("d");
+            genStack.isFull();
+            Console.WriteLine(genStack.ToString());
         }
     }
 }
